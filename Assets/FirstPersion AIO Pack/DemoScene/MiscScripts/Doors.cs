c61499de9da4b37f7f29168ae9e65a792ac5f890
SolidHinken <solidhinken@gmail.com>
2020-04-02 02:26:35 +0300

    02.04.20
    More assets
    Hierarchy organized
    SkyDome

--- Assets/FirstPersion AIO Pack/DemoScene/MiscScripts/Doors.cs
+++ Assets/FirstPersion AIO Pack/DemoScene/MiscScripts/Doors.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour {
 
-void OnTriggerEnter(Collider coll){
- if(coll.tag=="Player"){
-	 GetComponent<Animator>().Play("Door_open");
-	 this.enabled=false;
- }
+private void OnTriggerEnter(Collider coll)
+{
+    if(coll.CompareTag("Player"))
+    {
+	     GetComponent<Animator>().Play("Door_open");
+	     //this.enabled=false;
+    }
 }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Animator>().Play("Door_close");
+        //this.enabled = true;
+    }
 }
