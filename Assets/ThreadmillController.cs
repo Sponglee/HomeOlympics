eefe0cb872dc1833f4c8268b1a6f11ffd5bc5ea9
SolidHinken <solidhinken@gmail.com>
2020-04-12 22:11:55 +0300

    12.04.20
    Boxing coatstand rework

---
+++ Assets/ThreadmillController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreadmillController : ActivityControllerBase
+{
+
+    public float threadmillSpeed = 1f;
+    public Transform threadmillContent;
+
+
+    private void Update()
+    {
+        threadmillContent.Translate(threadmillSpeed * Time.deltaTime * Vector3.back /2f);
+    }
+    public override void DeInitializeActivity()
+    {
+      
+    }
+
+    public override void InitializeActivity()
+    {
+       
+    }
+}
---
+++ Assets/ThreadmillGridMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreadmillGridMover : MonoBehaviour
+{
+    [SerializeField] private ThreadmillController threadmillController;
+    public Material threadmillGrid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        threadmillGrid.mainTextureOffset += new Vector2(threadmillController.threadmillSpeed * Time.deltaTime,0);
+    }
+}
